77a9b72d1d0094b96ac0cdf80d69afa4d1451300
yaeli <yaeli@thoughtworks.com>
2019-06-20 01:18:23 +0800

    [yae][implement the function]

--- main/main.js
+++ main/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
+function getCountedItems(tags) {
+    return tags.reduce((allItems, currentIem) => {
+        if (currentIem.match("-")) {
+            const [key, count] = currentIem.split("-");
+            allItems[key] ? allItems[key] += parseFloat(count) : allItems[key] = parseFloat(count);
+        } else {
+            allItems[currentIem] ? allItems[currentIem]++ : allItems[currentIem] = 1;
+        }
+        return allItems;
+    }, {});
+}
+
 //TODO: 请在该文件中实现练习要求并删除此注释
+function printReceipt(tags) {
+    const countedItems = getCountedItems(tags);
+
+    let totalPrice = 0;
+    let savedPrice = 0;
+    const output = Object.keys(countedItems).map(barcode => {
+        const boughtIem = loadAllItems().find(item => item.barcode === barcode);
+        boughtIem.quantity = countedItems[barcode];
+        loadPromotions().forEach(promotion => {
+            if (promotion.type === 'BUY_TWO_GET_ONE_FREE') {
+                promotion.barcodes.forEach(promotionBarcode => {
+                    if (promotionBarcode === boughtIem.barcode && boughtIem.quantity > 2) {
+                        // boughtIem.computedQuantity = boughtIem.quantity - boughtIem.quantity/3;
+                        boughtIem.computedQuantity = boughtIem.quantity;
+                        boughtIem.computedQuantity--;
+                    }
+                })
+            }
+        });
+        boughtIem.subtotalPrice = (boughtIem.computedQuantity || boughtIem.quantity) * boughtIem.price;
+        totalPrice = totalPrice + boughtIem.subtotalPrice;
+        savedPrice = savedPrice + (boughtIem.computedQuantity ? (boughtIem.quantity - boughtIem.computedQuantity) * boughtIem.price : 0);
+        return boughtIem;
+    }).map(item =>
+        `名称：${item.name}，数量：${item.quantity}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.subtotalPrice.toFixed(2)}(元)`).join('\n');
+    console.log(`***<没钱赚商店>收据***
+${output}
+----------------------
+总计：${totalPrice.toFixed(2)}(元)
+节省：${savedPrice.toFixed(2)}(元)
+**********************`);
+}
